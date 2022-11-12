ed9b2d40815ceaf68f6cc9bd3727be533c2e29f6
Jimno <menaoir.jimmy@gmail.com>
2022-11-12 11:23:25 +0100

    Add feedback to help user playing further

--- master_mind.rb
+++ master_mind.rb
@@ -1,8 +1,9 @@
 class Matsermind
-  attr_reader :board
+  attr_reader :board, :feedback
 
   def initialize 
     @win_comb = [1, 6, 2, 4] # example
+    @feedback = []
     @board = [0, 0, 0, 0]
     p @board
   end
@@ -15,4 +16,24 @@ class Matsermind
     @win_comb == @board
   end
 
+  def feedback
+    board.each_with_index do |number, index|
+      @win_comb.each_with_index do |duplicate, index1|
+        if number == duplicate && index == index1
+          @feedback += ["*"]
+        elsif number == duplicate && index != index1
+          @feedback += ["~"]
+        end
+      end
+    end
+    p @feedback.shuffle
+  end
 end
+
+# example 
+game = Matsermind.new
+game.board
+game.update_board(1, 5, 6, 4)
+p game.board
+p game.win?
+game.feedback # => ["*", "*", "~"] result must be shuffled to make the game even harder!
